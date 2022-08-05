185efa5f176ff8cd936ffffac9bec422bea33396
Martin Starič <martin.staricc@gmail.com>
2022-08-05 18:38:32 +0200

    Swipe 2.0 and return to menu button

--- AwesomeBasketball/Assets/Scripts/BallScripts/moveBall.cs
+++ AwesomeBasketball/Assets/Scripts/BallScripts/moveBall.cs
@@ -14,7 +14,7 @@ public class moveBall : MonoBehaviour
     public static float CameraRotation;
     GameObject hoop;
     public static bool HoopCollision = false;
-
+    bool firstUpdate = false;
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
@@ -23,6 +23,10 @@ public class moveBall : MonoBehaviour
     void Update()
     {
         if(ThrowBegin){
+            if (!firstUpdate){
+                firstUpdate = true;
+                RotateSwipeDirection();
+            }
             Animation += Time.deltaTime;
             if(Animation<=1.5f && !HoopCollision){
                 currentPosition = GameObject.Find("ARCamera").transform.position;
@@ -32,7 +36,8 @@ public class moveBall : MonoBehaviour
             if(Animation>1.5f || HoopCollision){
                 transform.position = transform.position;
                 if(!HoopCollision){
-                    GetComponent<Rigidbody>().AddRelativeForce(0,0,7);
+                    Vector3 AfterAnimationDirection = new Vector3(0,0,7);
+                    GetComponent<Rigidbody>().AddRelativeForce(AfterAnimationDirection);
                 }
             }
             if(Animation>4f){
@@ -41,17 +46,22 @@ public class moveBall : MonoBehaviour
                 GameObject.Find("BallIndicator").GetComponent<MeshRenderer>().enabled = true;
                 GetComponent<MeshRenderer>().enabled = false;
                 HoopCollision = false;
+                firstUpdate = false;
             }
         }
     }
     void CreateParabolaAnimation(){
         //Requires minor tweaks
-        Vector3 hoopPosition = hoop.transform.position;// + new Vector3(0,1.3f,0.20f);
+        Vector3 hoopPosition = hoop.transform.position;
         float hoopBallDistance = Vector2.Distance(new Vector2(hoopPosition.x,hoopPosition.z),new Vector2(currentPosition.x,currentPosition.z));
-        float xCoordinate = (float)(hoopBallDistance*Math.Sin((Math.PI / 180) * CameraRotation))+currentPosition.x;
-        float zCoordinate = (float)(hoopBallDistance*Math.Cos((Math.PI / 180) * CameraRotation))+currentPosition.z;
+        float radianRotation = (float)(Math.PI / 180) * CameraRotation;
+        float xCoordinate = (float)(hoopBallDistance*Math.Sin(radianRotation))+currentPosition.x;
+        float zCoordinate = (float)(hoopBallDistance*Math.Cos(radianRotation))+currentPosition.z;
         Vector3 facingOffset = new Vector3(xCoordinate,0,zCoordinate) - swipeDirection/750;
         rigid.isKinematic = false;
         transform.position = MathParabola.Parabola(currentPosition,facingOffset+new Vector3(0,0.3f,-0.1f),0.7f,Animation/1.5f);
+    }
+    void RotateSwipeDirection(){
+        swipeDirection = Quaternion.Euler(0, CameraRotation, 0) * swipeDirection;
     }
 }
